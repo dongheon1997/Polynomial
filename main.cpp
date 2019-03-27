2d7f9dd0340ad94aab1a074553cd8ff4a1d231a0
dongheon1997 <48716569+dongheon1997@users.noreply.github.com>
2019-03-27 16:58:55 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Polynomial.h"
+
+using namespace std;
+
+int main() {
+	Polynomial P1, P2;
+	cout << "첫번째 다항식을 계수 지수 쌍으로 입력하세요. (지수가 0으로 입력되면 종료)" << endl;
+	P1.setTerm();
+	cout << "두번째 다항식을 계수 지수 쌍으로 입력하세요. (지수가 0으로 입력되면 종료)" << endl;
+	P2.setTerm();
+	cout << "두 다항식의 합은 " << P1 + P2 << endl;
+	cout << "두 다항식의 곱은 " << P1 * P2 << endl;
+}
