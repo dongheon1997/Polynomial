418b4a37a8bc039317c3e6095e17aa5e174ef93f
dongheon1997 <48716569+dongheon1997@users.noreply.github.com>
2019-03-27 16:58:49 +0900

    Create Polynomial.h

---
+++ Polynomial.h
@@ -0,0 +1,34 @@
+#pragma once
+
+class Term
+{
+	friend class Polynomial;
+private:
+	int coef, exp;
+
+public:
+	int getCoef() const;
+	int getExp() const;
+};
+
+//
+
+class Polynomial
+{
+	friend std::ostream &operator<<(std::ostream &os, const Polynomial &polynomial);
+private:
+	Term *termArr;
+	int capacity;
+	int terms;
+
+public:
+	Polynomial();
+	Polynomial(const Polynomial &other);
+	~Polynomial();
+	void resize();
+	void addTerm(int coef, int exp);
+	void setTerm();
+	Polynomial &operator=(const Polynomial &other);
+	const Polynomial operator+(const Polynomial &other) const;
+	const Polynomial operator*(const Polynomial &other) const;
+};
